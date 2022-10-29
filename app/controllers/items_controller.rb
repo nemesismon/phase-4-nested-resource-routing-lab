0a26ecf7d1143bd90545692370e5a900e0dfdf66
Michael <michael.w.kolb@gmail.com>
2022-10-28 21:29:31 -0400

    commit tree 764fbcb970f6d6d0dcec0f1b625ab13f2f008e01

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
+  
 
   def index
-    items = Item.all
-    render json: items, include: :user
+      user = User.find_by(id: params[:user_id])
+      users = User.all
+    if user
+      items = user.items
+      render json: items, include: :user, status: :not_found
+    elsif params[:user_id]
+      render json: {error: 'User not found'}, status: :not_found
+    else
+      items = Item.all
+      render json: items, include: :user, status: :ok
+    end
+  rescue
+    render json: {error: 'Items not found.'}, status: :not_found
+  end
+
+  def show
+    user = User.find(params[:user_id])
+    user_item = user.items.find(params[:id])
+    render json: user_item, include: :user, status: :ok
+  rescue
+    render json: {error: 'User item not found'}, status: :not_found
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    user = User.find(params[:id])
+    render json: user, include: :items, status: :ok
   end
 
 end
